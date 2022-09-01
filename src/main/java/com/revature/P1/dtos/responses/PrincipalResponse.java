984ab6f9e3c5122035e78c566536d59930d8e2ad
davidab99 <david.aburrington@gmail.com>
2022-09-01 14:58:39 -0400

    dtos responses and request

---
+++ src/main/java/com/revature/P1/dtos/responses/PrincipalResponse.java
@@ -0,0 +1,50 @@
+package com.revature.P1.dtos.responses;
+
+public class PrincipalResponse {
+    private String id;
+    private String username;
+    private String role;
+
+    public PrincipalResponse() {
+
+    }
+
+    public PrincipalResponse(String id, String username, String role) {
+        this.id = id;
+        this.username = username;
+        this.role = role;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
