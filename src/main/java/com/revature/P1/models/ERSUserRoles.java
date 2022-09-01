b33ea7ed7fd97a34a0ffd4fe214ded30c095f3e5
davidab99 <david.aburrington@gmail.com>
2022-09-01 14:50:31 -0400

    updated models

--- src/main/java/com/revature/P1/models/ERSUserRoles.java
+++ src/main/java/com/revature/P1/models/ERSUserRoles.java
@@ -1,4 +1,39 @@
 package com.revature.P1.models;
 
 public class ERSUserRoles {
+
+    private String roleID, role;
+
+    public ERSUserRoles(){
+
+    }
+
+    public ERSUserRoles(String roleID, String role) {
+        this.roleID = roleID;
+        this.role = role;
+    }
+
+    public String getRoleID() {
+        return roleID;
+    }
+
+    public void setRoleID(String roleID) {
+        this.roleID = roleID;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Roles{" +
+                "roleID='" + roleID + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/revature/P1/models/ERSUsers.java
+++ src/main/java/com/revature/P1/models/ERSUsers.java
@@ -1,4 +1,103 @@
 package com.revature.P1.models;
 
 public class ERSUsers {
+    private String uID;
+    private String uName, email, password, first, last;
+    private boolean isActive;
+    private String role;
+
+    public ERSUsers(){
+
+    }
+
+    public ERSUsers(String uID, String uName, String email, String password, String first, String last, boolean isActive, String role) {
+        this.uID = uID;
+        this.uName = uName;
+        this.email = email;
+        this.password = password;
+        this.first = first;
+        this.last = last;
+        this.isActive = isActive;
+        this.role = role;
+    }
+
+    public String getuID() {
+        return uID;
+    }
+
+    public void setuID(String uID) {
+        this.uID = uID;
+    }
+
+    public String getuName() {
+        return uName;
+    }
+
+    public void setuName(String uName) {
+        this.uName = uName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirst() {
+        return first;
+    }
+
+    public void setFirst(String first) {
+        this.first = first;
+    }
+
+    public String getLast() {
+        return last;
+    }
+
+    public void setLast(String last) {
+        this.last = last;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "uID='" + uID + '\'' +
+                ", uName='" + uName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", first='" + first + '\'' +
+                ", last='" + last + '\'' +
+                ", isActive=" + isActive +
+                ", role='" + role + '\'' +
+                '}';
+    }
 }
+
+
