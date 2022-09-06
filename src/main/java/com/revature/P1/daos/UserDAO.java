5e0a0475400a4c866aadc4b907e78d09e0e51590
davidab99 <david.aburrington@gmail.com>
2022-09-06 13:54:48 -0400

    updated daos and services

---
+++ src/main/java/com/revature/P1/daos/UserDAO.java
@@ -0,0 +1,116 @@
+package com.revature.P1.daos;
+
+import com.revature.P1.utils.custom_exceptions.InvalidSQLException;
+import com.revature.P1.models.ERSUsers;
+import com.revature.P1.utils.custom_exceptions.InvalidSQLException;
+import com.revature.P1.utils.database.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+public class UserDAO implements CrudDAO<ERSUsers> {
+    @Override
+    public void save(ERSUsers obj) {
+        try (Connection con = ConnectionFactory.getInstance().getConnection()) {
+            PreparedStatement ps = con.prepareStatement("INSERT INTO users (id, username, email, password, first, last, active, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+            ps.setString(1, obj.getuID());
+            ps.setString(2, obj.getuName());
+            ps.setString(3, obj.getEmail());
+            ps.setString(4, obj.getPassword());
+            ps.setString(5, obj.getFirst());
+            ps.setString(6, obj.getLast());
+            ps.setBoolean(7, obj.isActive());
+            ps.setString(8, obj.getRole());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new InvalidSQLException("An error occurred when tyring to save to the database.");
+        }
+    }
+
+    @Override
+    public void update(ERSUsers obj) {
+
+    }
+
+    @Override
+    public void delete(String id) {
+
+    }
+
+    @Override
+    public ERSUsers getById(String id) {
+        return null;
+    }
+
+    @Override
+    public List<ERSUsers> getAll() {
+        List<ERSUsers> userList = new ArrayList<>();
+
+        try (Connection con = ConnectionFactory.getInstance().getConnection()) {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM users");
+            ResultSet rs = ps.executeQuery();
+
+            while (rs.next()) {
+                ERSUsers user = new ERSUsers(rs.getString("id"), rs.getString("username"), rs.getString("email"), rs.getString("password"), rs.getString("first"), rs.getString("last"), rs.getBoolean("active"), rs.getString("role"));
+                userList.add(user);
+            }
+
+        } catch (SQLException e) {
+            throw new InvalidSQLException("An error occurred when tyring to save to the database.");
+        }
+
+        return userList;
+    }
+
+    public ERSUsers getUserByUsername(String username) {
+        try (Connection con = ConnectionFactory.getInstance().getConnection()) {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM users WHERE username = ?");
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return new ERSUsers(rs.getString("id"), rs.getString("username"), rs.getString("email"), rs.getString("password"), rs.getString("first"), rs.getString("last"), rs.getBoolean("active"), rs.getString("role"));
+            }
+
+        } catch (SQLException e) {
+            throw new InvalidSQLException("An error occurred when tyring to save to the database.");
+        }
+
+        return null;
+    }
+
+    public String getUsername(String username) {
+        try (Connection con = ConnectionFactory.getInstance().getConnection()) {
+            PreparedStatement ps = con.prepareStatement("SELECT (username) FROM users WHERE username = ?");
+            ps.setString(1, username);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) return rs.getString("username");
+
+        } catch (SQLException e) {
+            throw new InvalidSQLException("An error occurred when tyring to save to the database.");
+        }
+
+        return null;
+    }
+
+
+    public ERSUsers getUserByUsernameAndPassword(String username, String password) {
+        try (Connection con = ConnectionFactory.getInstance().getConnection()) {
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM users WHERE username = ? AND password = ?");
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next())
+                return new ERSUsers(rs.getString("id"), rs.getString("username"), rs.getString("email"), rs.getString("password"), rs.getString("first"), rs.getString("last"), rs.getBoolean("active"), rs.getString("role"));
+        } catch (SQLException e) {
+            throw new InvalidSQLException("An error occurred when tyring to save to the database.");
+        }
+
+        return null;
+    }
+}
